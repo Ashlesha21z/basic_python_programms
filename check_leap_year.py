34da913fe56f4f4e121910eeca85263a1b912c9e
Ashlesha21z <63294788+Ashlesha21z@users.noreply.github.com>
2023-01-20 22:38:25 +0530

    Update check_leap_year.py

--- check_leap_year.py
+++ check_leap_year.py
@@ -1,7 +1,8 @@
 
 
 def check_leap_year(year):
-    if (year%400 ==0) or (year%100 != 0) and year%4 == 0:
+    if (year % 400 ==0) or (year % 100 != 0)\
+    and year % 4 == 0:
         print("{} is a Leap Year".format(year))
     else:
         print("It is not a Leap Year")
